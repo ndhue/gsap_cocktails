046c09ad41302e3ed19ab0e9396020701aa1d810
Ngo Diem Hue <diemhuengo@gmail.com>
2025-11-20 12:05:54 +0700

    implement hero scroll animation

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,8 +1,17 @@
 import { useGSAP } from "@gsap/react"
 import gsap from "gsap";
 import { SplitText } from "gsap/all";
+import { useRef } from "react";
+import { useMediaQuery } from "react-responsive";
 
 const Hero = () => {
+  const videoRef = useRef();
+  // const videoTimelineRef = useRef();
+
+  const isMobile = useMediaQuery({
+    maxWidth: 767
+  });
+
   useGSAP(() => {
     const heroSplit = new SplitText('.title', { type: 'chars, words' });
     const paragraphSplit = new SplitText('.subtitle', { type: 'lines' });
@@ -35,6 +44,51 @@ const Hero = () => {
     })
       .to('.right-leaf', { y: 200 }, 0)
       .to('.left-leaf', { y: -200 }, 0);
+
+    const startValue = isMobile ? 'top 50%' : 'center 60%';
+    const endValue = isMobile ? '120% top' : 'bottom top';
+
+    // Video animation timeline
+    const videoTimelineRef = gsap.timeline({
+      scrollTrigger: {
+        trigger: 'video',
+        start: startValue,
+        end: endValue,
+        scrub: true,
+        pin: true,
+      }
+    });
+
+    videoRef.current.onloadedmetadata = () => {
+      videoTimelineRef.to(videoRef.current, {
+        currentTime: videoRef.current.duration
+      })
+    }
+
+    // other animation
+    // Video animation timeline
+    // videoTimelineRef.current = gsap.timeline({
+    //   scrollTrigger: {
+    //     trigger: '#hero',
+    //     start: startValue,
+    //     end: endValue,
+    //     scrub: true,
+    //     pin: true,
+    //     // other
+    //     onEnter: () => videoRef.current.play(),
+    //     onEnterBack: () => videoRef.current.play(),
+    //     onLeave: () => videoRef.current.pause(),
+    //     onLeaveBack: () => videoRef.current.pause(),
+    //   }
+    // })
+
+
+    // Update animation when the video metadata is loaded
+    // videoRef.current.onloadedmetadata = () => {
+    //   videoTimelineRef.current.to(videoRef.current, {
+    //     currentTime: videoRef.current.duration
+    //   })
+    // }
   }, []);
 
   return (
@@ -72,6 +126,18 @@ const Hero = () => {
           </div>
         </div>
       </section>
+
+      <div className="video absolute inset-0">
+        <video
+          ref={videoRef}
+          src="/videos/output.mp4"
+          muted
+          playsInline
+          preload="auto"
+        // autoPlay
+        // loop
+        />
+      </div>
     </>
   )
 }
